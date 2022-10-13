df777b50d2e24ef073245fad7b972a8a20746b35
thiagolcdeoliveira <thiagolocatellicdeoliveira@gmail.com>
2022-10-13 18:23:04 -0300

    teste
    teste

--- envia_email.php
+++ envia_email.php
@@ -1,43 +1,48 @@
 <?php
-// Multiple recipients
-$to = 'johny@example.com, sally@example.com'; // note the comma
-
-// Subject
-$subject = 'Birthday Reminders for August';
-
-// Message
-$message = '
-<html>
-<head>
-  <title>Birthday Reminders for August</title>
-</head>
-<body>
-  <p>Here are the birthdays upcoming in August!</p>
-  <table>
-    <tr>
-      <th>Person</th><th>Day</th><th>Month</th><th>Year</th>
-    </tr>
-    <tr>
-      <td>Johny</td><td>10th</td><td>August</td><td>1970</td>
-    </tr>
-    <tr>
-      <td>Sally</td><td>17th</td><td>August</td><td>1973</td>
-    </tr>
-  </table>
-</body>
-</html>
-';
-
-// To send HTML mail, the Content-type header must be set
-$headers[] = 'MIME-Version: 1.0';
-$headers[] = 'Content-type: text/html; charset=iso-8859-1';
-
-// Additional headers
-$headers[] = 'To: Thiago <thiagolocatellicdeoliveira@gmail.com>';
-$headers[] = 'From: Birthday Reminder <birthday@example.com>';
-$headers[] = 'Cc: birthdayarchive@example.com';
-$headers[] = 'Bcc: birthdaycheck@example.com';
-
-// Mail it
-mail($to, $subject, $message, implode("\r\n", $headers));
-?>
+require_once('PHPMailer/PHPMailer/src/PHPMailer.php');
+require_once('PHPMailer/PHPMailer/src/SMTP.php');
+require_once('PHPMailer/PHPMailer/src/Exception.php');
+ 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+ 
+$mail = new PHPMailer(true);
+ 
+try {
+	$mail->SMTPDebug = SMTP::DEBUG_SERVER;
+	$mail->isSMTP();
+	$mail->Host = 'smtp.gmail.com';
+	$mail->SMTPAuth = True;
+	$mail->Username = 'ti@araquari.sc.gov.br';
+	$mail->Password = '';
+	$mail->Port = 25;
+    $mail->SMTPSecure = 'tls';
+    $mail->CharSet = 'utf-8';  
+    $mail->SMTPSecure = false;
+   // $mail->SMTPAutoTLS = false;
+    
+    $mail->SMTPOptions = array(
+        'ssl' => array(
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+            'allow_self_signed' => True
+        )
+    );
+	$mail->setFrom('ti@araquari.sc.gov.br');
+	$mail->addAddress('thiagolocatellicdeoliveira@gmail.com.br');
+	//$mail->addAddress('endereco2@provedor.com.br');
+ 
+	$mail->isHTML(true);
+	$mail->Subject = 'Teste de email via gmail Canal TI';
+	$mail->Body = 'Chegou o email teste do <strong>Canal TI</strong>';
+	$mail->AltBody = 'Chegou o email teste do Canal TI';
+ 
+	if($mail->send()) {
+		echo 'Email enviado com sucesso';
+	} else {
+		echo 'Email nao enviado';
+	}
+} catch (Exception $e) {
+	echo "Erro ao enviar mensagem: {$mail->ErrorInfo}";
+}
