0181583e28ca0b4ff4079e6414a2b93390942045
thiagolcdeoliveira <thiagolocatellicdeoliveira@gmail.com>
2023-02-02 09:38:04 -0300

    AJustes redirect

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php 
+header("Location: /2023/ ");
+exit();
+
+require_once "header.php" ?>
+
+
+
+<div class="ui vertical stripe segment">
+
+	<div class="ui grid container">
+
+
+
+		
+
+	</div>
+</div>
+
+
+<?php require_once "footer.php" ?>
